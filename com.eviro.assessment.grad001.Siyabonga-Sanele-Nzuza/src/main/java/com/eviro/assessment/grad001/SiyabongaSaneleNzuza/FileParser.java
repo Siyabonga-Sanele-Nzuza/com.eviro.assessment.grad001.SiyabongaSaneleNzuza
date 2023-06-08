1a2a321dd6f6da1c7e5c1b212e7313962cc5a5c6
Siyabonga-Sanele-Nzuza <134116449+Siyabonga-Sanele-Nzuza@users.noreply.github.com>
2023-06-09 00:36:02 +0200

    Delete com.eviro.assessment.grad001.Siyabonga-Sanele-Nzuza directory

--- com.eviro.assessment.grad001.Siyabonga-Sanele-Nzuza/src/main/java/com/eviro/assessment/grad001/SiyabongaSaneleNzuza/FileParser.java
+++
@@ -1,9 +0,0 @@
-package com.eviro.assessment.grad001.SiyabongaSaneleNzuza;
-
-import java.io.File;
-import java.net.URI;
-public interface FileParser {
-    void parseCSV(File csvFile);
-    File convertCSVDataToImage(String base64ImageData);
-    URI createImageLink(File fileImage);
-}
--- com.eviro.assessment.grad001.Siyabonga-Sanele-Nzuza/src/main/java/com/eviro/assessment/grad001/SiyabongaSaneleNzuza/ImageController.java
+++
@@ -1,5 +0,0 @@
-package com.eviro.assessment.grad001.SiyabongaSaneleNzuza;
-
-
-public class ImageController {
-}
